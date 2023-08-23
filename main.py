7e87ed58e0e796ba1b319bbe3efaaaca92bc08db
zhenya <evgutaev@yandex.ru>
2023-08-23 21:37:46 +0300

    черепашка ползает

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-sweetsString = 'У Миши было '
-cakesString = ' и торта '
-sweetNum = 3
-cakesNum = 2
-print(sweetsString + str(sweetNum) + cakesString + str(cakesNum))
+import turtle
+import  time
+t = turtle.Pen()
+catokvodrat = 50 * 2
+ygol = 90
+t.color('dark magenta')
+t.pensize(3)
+#поднимаем черепашку
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.left(ygol * 2)
+t.forward(catokvodrat)
+t.left(ygol)
+t.forward(catokvodrat * 2)
+t.left(ygol)
+t.forward(catokvodrat)
+t.left(ygol)
+t.forward(catokvodrat)
+t.left(ygol)
+t.forward(catokvodrat)
+t.color('red')
+t.circle(catokvodrat / 2)
+t.fillcolor("green")
+
+t.begin_fill()
+t.left(45)
+t.forward(25)
+t.end_fill()
+time.sleep(5)
