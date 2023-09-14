cf77060b33c3c5863ad3d9646b598a83f3526d74
zhenya <evgutaev@yandex.ru>
2023-09-14 19:36:33 +0300

    tanle

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-import turtle
-import  time
-t = turtle.Pen()
-catokvodrat = 50 * 2
-ygol = 90
-t.color('dark magenta')
-t.pensize(3)
-#поднимаем черепашку
-t.penup()
-t.goto(0,200)
-t.pendown()
-t.left(ygol * 2)
-t.forward(catokvodrat)
-t.left(ygol)
-t.forward(catokvodrat * 2)
-t.left(ygol)
-t.forward(catokvodrat)
-t.left(ygol)
-t.forward(catokvodrat)
-t.left(ygol)
-t.forward(catokvodrat)
-t.color('red')
-t.circle(catokvodrat / 2)
-t.fillcolor("green")
-
-t.begin_fill()
-t.left(45)
-t.forward(25)
-t.end_fill()
-time.sleep(5)
+n = 9
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(i * j, " " * (5 - len(str(i * j))), end=" ")
+    print("\n", end="")
